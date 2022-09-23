7c9998518e27b76dca97fe3d7ff2487ad2e6f0f2
Pedro Arthur Alves de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2022-09-22 22:01:57 -0300

    First four items of semester day 1

---
+++ Exercicio3.java
@@ -0,0 +1,10 @@
+public class Exercicio3 
+{
+	public static void main(String[] args)
+	{
+		int resultado = 0;
+		OperacoesMatematicas optMat = new OperacoesMatematicas();
+		resultado = optMat.somar(5, 5);
+		System.out.println("Resultado: " + resultado);
+	}
+}
