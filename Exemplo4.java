2cc4e3f173a036554922dc89c0d88006cbbae34f
Lahat-Herev <pedroarthuralvesdeoliveira@hotmail.com>
2022-10-06 20:01:03 -0300

    First instance of exercise 4

--- Exemplo4.java
+++ Exemplo4.java
@@ -1,12 +1,20 @@
 public class Exemplo4 {
     public static void main(String[] args) {
         Integer valor1, valor2, res;
-        OperacoesMatematicas opMat = new OperacoesMatematicas();
+        OperacoesMatematicas optMat = new OperacoesMatematicas();
 
-        if (args.length < 2)
-        {
+        if (args.length < 2) {
             System.out.println("O programa precisa de 2 parâmetros");
             System.exit(0);
+        }
+
+        valor1 = Integer.parseInt(args[0]);
+        valor2 = Integer.parseInt(args[1]);
+
+        System.out.println("Primeiro teste");
+        if (valor1 < 5) {
+            res = optMat.multiplicar(valor1, valor2);
+            System.out.println(valor1 + " * " + valor2 + " = " + res);
         }
     }
 }
