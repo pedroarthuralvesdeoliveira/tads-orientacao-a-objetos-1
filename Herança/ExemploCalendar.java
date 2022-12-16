25d2dd5dc119fa2fa1797fb360ecc34d37f412a1
Lahat-Herev <pedroarthuralvesdeoliveira@hotmail.com>
2022-12-15 21:40:33 -0300

    Learning about dates

--- Herança/ExemploCalendar.java
+++ Herança/ExemploCalendar.java
@@ -1,4 +1,6 @@
+import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class ExemploCalendar {
     public static void main(String[] args) {
@@ -9,5 +11,34 @@ public class ExemploCalendar {
         System.out.println("\nVencimento: " + vencimento);
         vencimento.add(Calendar.MONTH, 1);
         System.out.println("\nNovo vencimento: " + vencimento);
+
+        GregorianCalendar gc = new GregorianCalendar();
+        System.out.println("\nAno: " + gc.get(Calendar.YEAR));
+        System.out.println("\nMês: " + gc.get(Calendar.MONTH));
+        System.out.println("\nDia do mês: " + gc.get(Calendar.DAY_OF_MONTH));
+        System.out.println("\nDia da semana: " + gc.get(Calendar.DAY_OF_WEEK));
+        System.out.println("\nDia do ano: " + gc.get(Calendar.DAY_OF_YEAR));
+        System.out.println("\nSemana do mês: " + gc.get(Calendar.WEEK_OF_MONTH));
+        System.out.println("\nDia da semana no mês: " + gc.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+        System.out.println("\nHora: " + gc.get(Calendar.HOUR));
+        System.out.println("\nAM/PM?: " + gc.get(Calendar.AM_PM));
+        System.out.println("\nHora do dia: " + gc.get(Calendar.HOUR_OF_DAY));
+        System.out.println("\nMinuto: " + gc.get(Calendar.MINUTE));
+        System.out.println("\nSegundo: " + gc.get(Calendar.SECOND));
+
+        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        GregorianCalendar data = new GregorianCalendar();
+        data.set(Calendar.YEAR, 2023);
+        data.set(Calendar.MONTH, Calendar.MAY);
+        data.set(Calendar.DAY_OF_MONTH, 1);
+        System.out.println(df.format(data.getTime()));
+
+        String dataNascimento = "02/03/2010";
+        String[] vetorDataNascimento = dataNascimento.split("/");
+        data.set(Calendar.YEAR, Integer.parseInt(vetorDataNascimento[2]));
+        data.set(Calendar.MONTH, Integer.parseInt(vetorDataNascimento[1]) - 1);
+        data.set(Calendar.DAY_OF_MONTH, Integer.parseInt(vetorDataNascimento[0]));
+
+        System.out.println(df.format(data.getTime()));
     }
 }
---
+++ Herança/ExemploLocalDateTime.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ExemploLocalDateTime {
+    public static void main(String[] args) {
+        LocalDate data = LocalDate.now();
+        System.out.println(data);
+
+        LocalTime hora = LocalTime.now();
+        System.out.println(hora);
+
+        LocalDateTime dataHora = LocalDateTime.now();
+        System.out.println(dataHora);
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String dataFormatada = dataHora.format(formato);
+
+        LocalDate hoje = LocalDate.now();
+        LocalDate amanha = hoje.plusDays(1);
+        LocalDate ontem = hoje.minusDays(1);
+        System.out.println("\nHoje: " + hoje);
+        System.out.println("\nAmanha: " + amanha);
+        System.out.println("\nOntem: " + ontem);
+
+        LocalTime horaAtual = LocalTime.now();
+        LocalTime menosUmaHora = horaAtual.minusHours(1);
+        System.out.println("\nHora atual: " + horaAtual);
+        System.out.println("\nMenos uma hora: " + menosUmaHora);
+
+        LocalDateTime agora = LocalDateTime.now();
+        LocalDateTime futuro = agora.plusDays(5).plusHours(3).plusMinutes(8);
+        System.out.println("\n " + agora.format(formato));
+        System.out.println("\n " + futuro.format(formato));
+
+        DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate minhaData = LocalDate.parse("21/02/2023", formatoData);
+        System.out.println("Minha data: " + minhaData);
+        System.out.println("Minha data formatada: " + minhaData.format(formatoData));
+    }
+}
